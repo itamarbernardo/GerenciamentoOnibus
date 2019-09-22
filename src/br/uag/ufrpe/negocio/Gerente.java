331d1cc37ea747366bb2ecef6018e69caff7db1a
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-09-22 00:36:09 -0300

    Add files via upload

--- src/br/uag/ufrpe/negocio/Gerente.java
+++ src/br/uag/ufrpe/negocio/Gerente.java
@@ -11,10 +11,57 @@ public class Gerente {
     private String cpf;
     private String identificador;
     private String telefone;
-    private Endereco endereço;
+    private Endereco endereco;
     private LocalDate dataNasc;
     
     
+    public String getNome(){
+        return nome;
+    }
     
+    public void setNome(String nome){
+        this.nome = nome;
+    }
     
+    public String getCpf(){
+        return cpf;
+    }
+    
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    
+    public String getIdentificador(){
+        return identificador;
+    }
+    
+    public void setIdentificador(String identificador){
+        this.identificador = identificador;
+    }
+    
+    public String getTelefone(){
+        return telefone;
+    }
+    
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    
+    public Endereco getEndereco(){
+        return endereco;
+    }
+    
+    public void setEndereco(Endereco endereco){
+        this.endereco = endereco;
+    }
+    
+    public LocalDate getDataNasc(){
+        return dataNasc;
+    }
+    
+    public void setDatanasc(LocalDate dataNasc){
+        this.dataNasc = dataNasc;
+    }
+    
+
 }
