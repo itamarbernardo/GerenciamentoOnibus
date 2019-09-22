670ddbf6b454a046c764b3d89f8c34dd3f49669e
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-09-22 00:35:06 -0300

    Add files via upload
    Ainda Concluindo

---
+++ src/br/uag/ufrpe/negocio/Gerente.java
@@ -0,0 +1,20 @@
+package gerenciamentodeviagens;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author Jackson
+ */
+public class Gerente {
+    
+    private String nome;
+    private String cpf;
+    private String identificador;
+    private String telefone;
+    private Endereco endereço;
+    private LocalDate dataNasc;
+    
+    
+    
+    
+}
