0e9b168129d8c4fbc1a5e4ec4f1bcf3158cb3493
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-09-24 16:33:18 -0300

    Calcular idade em Passageiro

--- src/br/uag/ufrpe/negocio/Passageiro.java
+++ src/br/uag/ufrpe/negocio/Passageiro.java
@@ -6,18 +6,29 @@
 
 package br.uag.ufrpe.negocio;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
- * @author Itamar Jr
+ * @author Jackson / Itamar Jr
  */
 public class Passageiro {
+    private String nomeCompleto;
     private String dataNascimento;
     private boolean possuiIdJovem;
     private String cpf;
-
-    public Passageiro(boolean possuiIdJovem, String cpf) {
+    private String rg;
+    
+    public Passageiro(String nomeCompleto, String dataNascimento, boolean possuiIdJovem, String cpf, String rg) {
+        this.nomeCompleto = nomeCompleto;
+        this.dataNascimento = dataNascimento;
         this.possuiIdJovem = possuiIdJovem;
         this.cpf = cpf;
+        this.rg = rg;
     }
 
     public String getCpf() {
@@ -27,8 +38,6 @@ public class Passageiro {
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
-    
     
     public int calcularIdade(){
         return 20;
@@ -50,6 +59,48 @@ public class Passageiro {
         this.possuiIdJovem = possuiIdJovem;
     }
 
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    public static int calculaIdade(String dataNascimento, String pattern){
+        //Calcula a Idade baseado em String. Exemplo: calculaIdade("20/08/1977","dd/MM/yyyy");
+        DateFormat sdf = new SimpleDateFormat(pattern);
+        Date dataNascInput = null;
+
+        try {
+            dataNascInput= sdf.parse(dataNascimento);
+
+        } catch (Exception e) {}
+            Calendar dateOfBirth = new GregorianCalendar();
+            dateOfBirth.setTime(dataNascInput);
+
+        // Cria um objeto calendar com a data atual
+        Calendar today = Calendar.getInstance();
+
+        // Obtém a idade baseado no ano
+        int idade = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
+        dateOfBirth.add(Calendar.YEAR, idade);
+
+        if (today.before(dateOfBirth)) {
+            idade--;
+        }
+
+        return idade;
+        }
+        
     @Override
     public String toString() {
         return "Passageiro{" + "dataNascimento=" + dataNascimento + ", possuiIdJovem=" + possuiIdJovem + '}';
