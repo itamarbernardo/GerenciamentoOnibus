bdb5bae2c31a254260e0242ee43dfff1ee68466d
Gabriel Felix <53801722+Bielfla27@users.noreply.github.com>
2019-09-24 17:01:44 -0300

    Add files via upload

---
+++ src/br/uag/ufrpe/negocio/Pessoa.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Pessoa {
+    private String nomeCompleto;
+    private String cpf;
+    private String rg;
+    private String email; 
+    private String login;
+    private Endereco endereco;
+
+    public Pessoa(String nomeCompleto, String cpf, String rg, String email, String login,Endereco endereco) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.email = email;
+        this.login = login;
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the nomeCompleto
+     */
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    /**
+     * @param nomeCompleto the nomeCompleto to set
+     */
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+
+   
+}
