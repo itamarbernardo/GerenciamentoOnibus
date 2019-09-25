f2b62d4f8907ab90fae5c1579eff05a9d1a65dfe
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-09-25 00:25:31 -0300

    Add files via upload

--- src/br/uag/ufrpe/negocio/Passageiro.java
+++ src/br/uag/ufrpe/negocio/Passageiro.java
@@ -6,11 +6,8 @@
 
 package br.uag.ufrpe.negocio;
 
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import static br.uag.ufrpe.negocio.Data.converterDataParaLocalDate;
+import java.time.LocalDate;
 
 /**
  *
@@ -25,8 +22,7 @@ public class Passageiro extends Pessoa {
         super(nomeCompleto, cpf, rg, email, endereco, telefone);
         
         this.dataNascimento = dataNascimento;
-        this.possuiIdJovem = possuiIdJovem;
-        
+        this.possuiIdJovem = possuiIdJovem;    
     }
        
     public String getDataNascimento() {
@@ -45,31 +41,11 @@ public class Passageiro extends Pessoa {
         this.possuiIdJovem = possuiIdJovem;
     }
     
-    public int calcularIdade(String dataNascimento, String pattern){
-        //Calcula a Idade baseado em String. Exemplo: calculaIdade("20/08/1977","dd/MM/yyyy");
-        DateFormat sdf = new SimpleDateFormat(pattern);
-        Date dataNascInput = null;
-
-        try {
-            dataNascInput= sdf.parse(dataNascimento);
-
-        } catch (Exception e) {}
-            Calendar dateOfBirth = new GregorianCalendar();
-            dateOfBirth.setTime(dataNascInput);
-
-        // Cria um objeto calendar com a data atual
-        Calendar today = Calendar.getInstance();
-
-        // Obtém a idade baseado no ano
-        int idade = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
-        dateOfBirth.add(Calendar.YEAR, idade);
-
-        if (today.before(dateOfBirth)) {
-            idade--;
-        }
-
-        return idade;
-        }
+    public int calcularIdade(){
+        LocalDate data = converterDataParaLocalDate(this.dataNascimento);
+        LocalDate localDate = LocalDate.now();
+        return localDate.getYear() - data.getYear();
+    }
         
     @Override
     public String toString() {
