d054856eaf9566321b187c97e79d3c1ba5b4a3c2
Miyyu <49696525+Miyyu@users.noreply.github.com>
2019-09-27 18:47:16 -0300

    Update RepositorioOnibus.java

--- src/br/uag/ufrpe/repositorios/RepositorioOnibus.java
+++ src/br/uag/ufrpe/repositorios/RepositorioOnibus.java
@@ -5,10 +5,64 @@
  */
 package br.uag.ufrpe.repositorios;
 
+import br.uag.ufrpe.negocio.Onibus;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author miyyu
+ * @author Emily Santos;
  */
 public class RepositorioOnibus {
+        private List<Onibus> onibus;
+    
+    
+    public RepositorioOnibus(){
+        onibus = new ArrayList<>();
+    }        
+    
+    // ----------------------------- METODOS ---------------------------------------
+    
+    public boolean adicionarOnibus(Onibus oni){
+        if(procurarOnibus(oni.getPlaca()) != null){
+            onibus.add(oni);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean alterarOnibus(Onibus oni, String placa){
+        Onibus o = procurarOnibus(placa);
+        if(o != null){
+            onibus.remove(o);
+            adicionarOnibus(oni);
+            return true;
+        }
+        return false;
+    }
+    
+    public Onibus procurarOnibus(String placa){
+        for(Onibus o: onibus){
+            if(o.getPlaca() == placa) {
+                return o;
+            }
+        }
+        return null;
+    }
+    
+    public boolean removerOnibus(String placa){
+        Onibus o = procurarOnibus(placa);
+        
+        if(o != null){
+            onibus.remove(o);
+            return true;
+        }
+        return false;
+    }
+   
+    public boolean removerOnibus(Onibus o){
+        onibus.remove(o);
+        return true;
+    }
     
 }
