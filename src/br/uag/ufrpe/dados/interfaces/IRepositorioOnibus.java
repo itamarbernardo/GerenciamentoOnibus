64144c974b1ff0bcbaafbcae744590a1237038e4
Miyyu <49696525+Miyyu@users.noreply.github.com>
2019-11-19 21:43:01 -0300

    Update IRepositorioOnibus.java

--- src/br/uag/ufrpe/dados/interfaces/IRepositorioOnibus.java
+++ src/br/uag/ufrpe/dados/interfaces/IRepositorioOnibus.java
@@ -11,6 +11,7 @@ public interface IRepositorioOnibus {
     void adicionarOnibus(Onibus oni);
     void alterarOnibus(Onibus oni);
     Onibus procurarOnibus(String placa);
+    Onibus procurarOnibus(Onibus oni);
     void removerOnibus(String placa);
     void removerOnibus(Onibus o);
     List<Onibus> listagemOnibus();
